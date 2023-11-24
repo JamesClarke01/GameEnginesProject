2c7faff571072f437264a620d03b398d83efdc2c
James Clarke <jamesclarkedublin@gmail.com>
2023-11-24 18:29:58 +0000

    new bolt physics

--- bolt.gd
+++ bolt.gd
@@ -1,9 +1,14 @@
 extends Node3D
-#@export var linearVelocity: Vector3;
+
+@export var speed = 5.0
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():	
+	
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,7 +16,29 @@ func _process(delta):
 	#linearVelocity = get_node("RigidBody3D").linear_velocity
 	destroyIfOutOfBounds()
 	pass
+
+
+
+
+func _physics_process(delta):
+	# Local space forward
 	
+	var target = $"../Target"
+	#print(target.position)
+	#var forward = (target.position - position).normalized()
+	var forward = Vector3.BACK	
+	var v  = speed * forward * delta		
+	
+	# Transform to world space by multiplying by
+	#var q = global_transform.basis.get_rotation_quaternion() 
+	#v = q * v
+	
+	global_position += v
+	
+
+
+
+
 func destroyIfOutOfBounds(): 	#Not Working
 	#boundaries of scene
 	const X_LOW_BOUND = -50
@@ -30,3 +57,8 @@ func destroyIfOutOfBounds(): 	#Not Working
 		global_position.z > Z_HIGH_BOUND):
 			print("Destroy")
 			queue_free()	
+
+"""
+func _on_rigid_body_3d_body_entered(body):
+	print("Body entered")	
+"""
