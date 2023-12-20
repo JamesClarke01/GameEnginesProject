21df482264386641e946b369a51382ae9b4a5217
MossDog <lukejohnmosshughes@gmail.com>
2023-12-20 11:31:30 +0000

    improved start room

--- start_room.gd
+++ start_room.gd
@@ -6,22 +6,26 @@ var phraseTimer
 var textIndex
 var phraseIndex
 var r2Dialogue
+var r2Sound
+var opening
+var floor
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	r2Speech = $R2D2/Speech
 	textTimer = $TextTimer
 	phraseTimer = $PhraseTimer
+	r2Sound = $R2D2/R2Sound
+	floor = $Room/Floor
 	textIndex = 0
 	phraseIndex = 0
-	r2Dialogue = ["The last guy that went in there\ndidn't last very long...", "Are you sure you want to do this?", "Well... If you say so.", "Go ahead and hit that lever\nwhen you're ready.\nGood luck!"]
-
-	textTimer.start()
-	
+	r2Dialogue = ["The last guy that went in there\ndidn't last very long...", "Are you sure you want to do this?", "Well... If you say so.", "Go ahead and hit that green button\nwhen you're ready.\nGood luck!"]
+	opening = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if opening == true and floor.global_position.x > -25:
+		floor.global_position.x += 5 * Vector3(1,0,0) * delta;
 
 func _on_text_timer_timeout():
 	if textIndex < len(r2Dialogue[phraseIndex]):
@@ -32,8 +36,17 @@ func _on_text_timer_timeout():
 		textIndex = 0
 		textTimer.stop()
 		phraseTimer.start()
+	else:
+		textTimer.stop()
+		phraseTimer.stop()
 
 
 func _on_phrase_timer_timeout():
 	textTimer.start()
+	r2Sound.play()
 	phraseTimer.stop()
+
+
+func _on_button_press_body_entered(body):
+	opening = true
+	
